608563f6147cff2ac2303417890ce5823cab9b74
sara <sara.ramo177@gmail.com>
2025-01-04 16:14:16 +0100

    vector.c

---
+++ vector.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vector.h"
+#include "matrix.h"
+
+/** create a (zero filled) N dimensional double vector.
+ *  @param n the length of the vector 0<n
+ *  @return a N-dimensional double vector reference
+ */
+vector createVector(unsigned short n){
+    
+}
