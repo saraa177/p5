dbe2dd3447bbfd926c663f9a4fd2907146e3cd76
sara <sara.ramo177@gmail.com>
2025-01-07 11:43:31 +0100

    vector.c

--- vector.c
+++ vector.c
@@ -18,14 +18,13 @@ vector createVector(unsigned short n){
         exit(-1);
     }
 
-    v = (vector)malloc(sizeof(struct vector_struct));
+    v = (vector)calloc(1, sizeof(struct vector_struct) + n * sizeof(double));
     if (v == NULL) {
         exit(-1);
     }
 
     v->length = n;
-
-    v->data = (double*)calloc(n, sizeof(double));
+    v->data = (double *)v + 1;
     if (v->data == NULL) {
         printf("Error: Memory allocation failed for vector data.\n");
         free(v);
@@ -40,13 +39,7 @@ vector createVector(unsigned short n){
  *  @param v valid vector reference
  */
 void rmVector(vector v){
-    if (v != NULL) {
-        if (v->data != NULL) {
-            free(v->data);  
-            v->data = NULL;
-        }
-        free(v);  /* Free the vector struct itself */
-    }
+    free(v);
 }
 
 /** @return length N of the vector */
@@ -91,12 +84,12 @@ double getValue(vector v, unsigned short j){
 void setValue(vector v, unsigned short j, double value){
     if (v == NULL || v->data == NULL) {
         printf("Error: Vector data is NULL. Cannot set value.\n");
-        exit(1);
+        exit(-1);
     }
 
     if (j >= v->length) {
         printf("Error: Index %u is out of bounds. Valid range: 0 to %u.\n", j, v->length - 1);
-        exit(1);
+        exit(-1);
     }
 
     v->data[j] = value;
@@ -149,57 +142,10 @@ vector vectorPlusVector(vector a, vector b){
         exit(1);  
     }
 
-    c = createVector(a->length);  /* Reuse the createVector function to allocate a new vector */
+    c = createVector(a->length);
     for (i = 0; i < a->length; i++) {
         c->data[i] = a->data[i] + b->data[i];
     }
 
     return c;
-}
-
-int main(void) {
-    
-    vector v1 = createVector(3);
-    vector v2 = createVector(3);
-
-    
-    setValue(v1, 0, 1.0);
-    setValue(v1, 1, 2.0);
-    setValue(v1, 2, 3.0);
-
-    
-    setValue(v2, 0, 4.0);
-    setValue(v2, 1, 5.0);
-    setValue(v2, 2, 6.0);
-
-    
-    printf("Vector 1: ");
-    for (int i = 0; i < size(v1); i++) {
-        printf("%f ", getValue(v1, i));
-    }
-    printf("\n");
-
-    printf("Vector 2: ");
-    for (int i = 0; i < size(v2); i++) {
-        printf("%f ", getValue(v2, i));
-    }
-    printf("\n");
-
-    double dot_product = vectorDotVector(v1, v2);
-    printf("Scalar Product (v1 . v2): %f\n", dot_product);
-
-    
-    vector v3 = vectorPlusVector(v1, v2);
-    printf("Vector Addition (v1 + v2): ");
-    for (int i = 0; i < size(v3); i++) {
-        printf("%f ", getValue(v3, i));
-    }
-    printf("\n");
-
-    
-    rmVector(v1);
-    rmVector(v2);
-    rmVector(v3);
-
-    return 0;
 }
