c417c7a0da7df73c642c310de17997f83d3a30b9
sara <sara.ramo177@gmail.com>
2025-01-07 11:44:04 +0100

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "vector.h"
 #include "matrix.h"
 
 struct matrix_struct {
@@ -9,114 +8,45 @@ struct matrix_struct {
     double** data;
 };
 
-struct vector_struct {
-    unsigned int length;
-    double* data;
-};
-
-
-typedef struct matrix_struct* matrix;
-typedef struct vector_struct* vector;
-
 /** create a (zero filled) NxM double matrix. */
 matrix createMatrix(unsigned short n, unsigned short m) {
-    unsigned int i;
-    unsigned int j;
-    matrix a;
-    if (n == 0 || m == 0) {
-        return NULL;
-    }
-
-    a = (matrix)malloc(sizeof(struct matrix_struct));
-    if (a == NULL) return NULL;
-
-    a->rows = n;
-    a->cols = m;
-
-    
-    a->data = (double**)malloc(n * sizeof(double*));
-    if (a->data == NULL) {
-        free(a); 
-        return NULL;
-    }
+    matrix mat = calloc(1, sizeof(struct matrix_struct) + n * sizeof(double *) + m * n * sizeof(double));
+    unsigned short i;
+    mat->rows = n;
+    mat->cols = m;
+    mat->data = (double **)(mat + 1);
 
     for (i = 0; i < n; i++) {
-        a->data[i] = (double*)malloc(m * sizeof(double));
-        if (a->data[i] == NULL) {
-            
-            for (j = 0; j < i; j++) {
-                free(a->data[j]);
-            }
-            free(a->data);
-            free(a);
-            return NULL;
-        }
+        mat->data[i] = (double *)mat->data + n + i * m;
     }
-
-    // Fill the matrix with zeros
-    for (i = 0; i < n; i++) {
-        for (unsigned int j = 0; j < m; j++) {
-            a->data[i][j] = 0.0;
-        }
-    }
-
-    return a;
+    return mat;
 }
 
 /** remove a matrix freeing allocated memory. */
 void rmMatrix(matrix a) {
-    if (a != NULL) {
-        for (unsigned int i = 0; i < a->rows; i++) {
-            free(a->data[i]);
-        }
-        free(a->data);
-        free(a);
-    }
+    free(a);
 }
 
 /** return the number of matrix rows. */
 unsigned short rows(matrix a) {
-    if (a != NULL) {
-        return (a->rows);
-    }
-    return 0;
+    return a->rows;
 }
 
 /** return the number of matrix columns. */
 unsigned short cols(matrix a) {
-    if (a != NULL) {
-        return (a->cols);
-    }
-    return 0;
+    return a->cols;
 }
 
 /** access to the internal NxM data. */
 double** data(matrix a) {
-    if (a == NULL)
-        return NULL;
-
-    
-    double** result = (double**)malloc(a->rows * sizeof(double*));
-    if (result == NULL) {
-        return NULL;
-    }
-
-    for (unsigned int i = 0; i < a->rows; i++) {
-        result[i] = a->data[i];
-    }
-
-    return result;
+    return a->data;
 }
 
 /** get the matrix entry with index r,c */
 double getEntry(matrix a, unsigned short r, unsigned short c) {
-    if (a == NULL) {
-        printf("Error: Matrix is NULL.\n");
-        exit(-1);
-    }
 
     if (r >= a->rows || c >= a->cols) {
-        printf("Error: Invalid row (%hu) or column (%hu) index.\n", r, c);
+        printf("Error: Wrong dimensions for matrix");
         exit(-1);
     }
 
@@ -147,26 +77,16 @@ matrix matrixDotMatrix(matrix a, matrix b) {
         exit(-1);
     }
 
-    if (a->cols != b->rows) {
+    if (a->cols != b->rows || a->rows != b->cols) {
         printf("Error: Matrix dimensions do not match for multiplication.\n");
         exit(-1);
     }
 
-    c = (matrix)malloc(sizeof(struct matrix_struct));
+    c = createMatrix(a->rows, b->cols);
     if (c == NULL) {
         printf("Error: Memory allocation failed for result matrix.\n");
         exit(-1);
     }
-
-    c->rows = a->rows;
-    c->cols = b->cols;
-
-    
-    c->data = (double**)malloc(c->rows * sizeof(double*));
-    for (i = 0; i < c->rows; i++) {
-        c->data[i] = (double*)malloc(c->cols * sizeof(double));
-    }
-
     
     for (i = 0; i < c->rows; i++) {
         for (j = 0; j < c->cols; j++) {
@@ -194,19 +114,10 @@ matrix matrixPlusMatrix(matrix a, matrix b) {
         exit(-1);
     }
 
-    c = (matrix)malloc(sizeof(struct matrix_struct));
+    c = createMatrix(a->rows, a->cols);
     if (c == NULL) {
         printf("Error: Memory allocation failed for result matrix.\n");
         exit(-1);
-    }
-
-    c->rows = a->rows;
-    c->cols = a->cols;
-
-    
-    c->data = (double**)malloc(c->rows * sizeof(double*));
-    for (i = 0; i < c->rows; i++) {
-        c->data[i] = (double*)malloc(c->cols * sizeof(double));
     }
 
     for (i = 0; i < c->rows; i++) {
@@ -227,28 +138,21 @@ vector matrixDotVector(matrix a, vector x) {
         exit(-1);
     }
 
-    if (a->cols != x->length) {
+    if (a->cols != size(x)) {
         printf("Error: Matrix dimensions do not match for multiplication.\n");
         exit(-1);
     }
 
-    y = (vector)malloc(sizeof(struct vector_struct));
+    y = createVector(a->rows);
     if (y == NULL) {
         printf("Error: Memory allocation failed for result vector.\n");
         exit(-1);
     }
 
-    y->length = a->rows;
-    y->data = (double*)malloc(y->length * sizeof(double));
-    if (y->data == NULL) {
-        printf("Error: Memory allocation failed for result vector data.\n");
-        exit(-1);
-    }
-
     for (i = 0; i < a->rows; i++) {
-        y->data[i] = 0;
+        setValue(y, i, 0);
         for (j = 0; j < a->cols; j++) {
-            y->data[i] += a->data[i][j] * x->data[j];
+            setValue(y, i, getValue(y, i) + a->data[i][j] * getValue(x, j));
         }
     }
 
